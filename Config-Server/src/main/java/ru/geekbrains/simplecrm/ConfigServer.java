b3bb7eea2f64420acdd6ca7568917796d1190d83
griz <grizlen@rambler.ru>
2021-12-11 23:16:34 +0500

    init

---
+++ Config-Server/src/main/java/ru/geekbrains/simplecrm/ConfigServer.java
@@ -0,0 +1,9 @@
+package ru.geekbrains.simplecrm;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServer {
+}
